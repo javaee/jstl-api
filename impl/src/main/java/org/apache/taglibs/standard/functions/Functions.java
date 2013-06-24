bd0b1e6e74302fd89e418654def035688bdda2c5
Kin Man Chung <kinman.chung@oracle.com>
2013-06-24 23:15:38 +0000

    Issue https://java.net/jira/browse/JSTL-9
    svn path=/trunk/; revision=95

--- impl/src/main/java/org/apache/taglibs/standard/functions/Functions.java
+++ impl/src/main/java/org/apache/taglibs/standard/functions/Functions.java
@@ -125,10 +125,7 @@ public class Functions {
     public static boolean endsWith(String input, String substring) {
         if (input == null) input = "";
         if (substring == null) substring = "";
-        int index = input.indexOf(substring);
-        if (index == -1) return false;
-        if (index == 0 && substring.length() == 0) return true;
-        return (index == input.length() - substring.length());
+        return input.endsWith(substring);
     }  
     
     public static String substring(String input, int beginIndex, int endIndex) {
